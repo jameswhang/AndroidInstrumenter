a7fb26dbd7b5f8f2d3383633e59621cac7ff82a0
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2017-05-09 00:27:45 -0500

    Add URL extraction and findViewById parameters

--- AndroidInstrument.java
+++ AndroidInstrument.java
@@ -13,6 +13,8 @@ public class AndroidInstrument {
         //prefer Android APK files// -src-prec apk
         Options.v().set_src_prec(Options.src_prec_apk);
 
+        Options.v().set_allow_phantom_refs(true);
+
         //output as APK, too//-f J
 //        Options.v().set_output_format(Options.output_format_dex);
 
@@ -43,57 +45,249 @@ public class AndroidInstrument {
                     final Unit u = iter.next();
                     u.apply(new AbstractStmtSwitch() {
 
+                        public void caseAssignStmt(AssignStmt stmt) {
+                            if (stmt.containsInvokeExpr()) {
+                                InvokeExpr invokeExpr = stmt.getInvokeExpr();
+
+                                if (invokeExpr.getMethod().getName().contains("findViewById")) {
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpRef2 = addTmpRef(b);
+                                    Local tmpString = addTmpString(b);
+
+                                    Scene.v().addBasicClass("java.lang.System",SootClass.SIGNATURES);
+
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("[NUDEBUG] findViewById called with ID: ")), u);
+
+                                    // insert "tmpRef.println(tmpString);"
+                                    SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(int)");
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), invokeExpr.getArg(0))), u);
+
+                                    //check that we did not mess up the Jimple
+                                    b.validate();
+                                }
+                            }
+                        }
+
+
                         public void caseInvokeStmt(InvokeStmt stmt) {
                             InvokeExpr invokeExpr = stmt.getInvokeExpr();
+                            System.out.println(stmt.toString());
                             if(invokeExpr.getMethod().getName().contains("setContentView")) {
+                                if (invokeExpr.getArg(0).getType().toString().equals("int")) {
+
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpRef2 = addTmpRef(b);
+                                    Local tmpString = addTmpString(b);
+
+                                    Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
+
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+
+                                    // insert "tmpLong = 'HELLO';"
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("[NUDEBUG] setContentView called with ID: ")), u);
+
+                                    // insert "tmpRef.println(tmpString);"
+                                    //SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.Integer)");
+                                    SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(int)");
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), invokeExpr.getArg(0))), u);
+
+                                    //check that we did not mess up the Jimple
+                                    b.validate();
+                                } else if (invokeExpr.getArg(0).getType().toString().contains("android.view.View")) {
+                                    // TODO
+                                    /*
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpRef2 = addTmpRef(b);
+                                    Local tmpInt = addTmpInt(b);
+                                    Local tmpString = addTmpString(b);
+                                    Local localVal = null;
+
+                                    Value argVal = invokeExpr.getArg(0);
+                                    if (!(argVal instanceof Local)) {
+                                         localVal = (Local)argVal;
+                                    }
+
+
+                                    Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
+                                    Scene.v().addBasicClass("android.view.View", SootClass.SIGNATURES);
+
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+
+                                    // insert "tmpLong = 'HELLO';"
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("[NUDEBUG] setContentView called with ID: ")), u);
+
+                                    System.out.println("ANDROID VIEW METHODS COUNT: " + Scene.v().getSootClass("Android.view.View").getMethodCount());
+                                    for (SootMethod m : Scene.v().getSootClass("Android.view.View").getMethods()) {
+                                        System.out.println("************" + m.toString());
+                                    }
+
+
+                                    //SootMethod getId = Scene.v().getSootClass("Android.view.View").getMethod("int getId()");
+
+                                    //units.insertBefore(Jimple.v().newAssignStmt(tmpInt,
+                                    //        Jimple.v().newVirtualInvokeExpr(localVal, getId.makeRef())), u);
+
+
+                                    // insert "tmpRef.println(tmpString);"
+                                    //SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.Integer)");
+                                    SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(int)");
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), invokeExpr.getArg(0))), u);
+
+                                    //check that we did not mess up the Jimple
+                                    b.validate();
+                                    */
+                                }
+                            } else if (invokeExpr.toString().contains("java.net.URL") && invokeExpr.getMethod().toString().contains("init")) {
+                                System.out.println("[NUDEBUG] " + invokeExpr.toString());
+                                SootMethod m = invokeExpr.getMethod();
+                                Value protocol = null;
+                                Value v = null;
+
+                                if (m.getParameterCount() == 1) { // URL(String spec)
+                                    v = invokeExpr.getArg(0);
+                                } else if (m.getParameterCount() == 2) { //URL(URL context, String spec)
+                                    v = invokeExpr.getArg(1);
+                                } else if (m.getParameterCount() == 3) {
+                                    // URL(String protocol, String host, String file),  or
+                                    // URL(URL context, String spec, URLStreamHandler handler)
+                                    v = invokeExpr.getArg(1);
+                                } else if (m.getParameterCount() == 4) {
+                                    // URL(String protocol, String host, int port, String file)
+                                    v = invokeExpr.getArg(1);
+                                } else if (m.getParameterCount() == 5) {
+                                    // URL(String protocol, String host, int port, String file, URLStreamHandler handler)
+                                    v = invokeExpr.getArg(1);
+                                }
 
                                 Local tmpRef = addTmpRef(b);
                                 Local tmpRef2 = addTmpRef(b);
                                 Local tmpString = addTmpString(b);
 
-                                String argStr = "";
-
-
-                                for (Value v : invokeExpr.getArgs()) {
-                                    argStr += v.toString() + "(" + v.getType() + "), " ;
-                                }
-
-                                Scene.v().addBasicClass("java.lang.System",SootClass.SIGNATURES);
+                                Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
 
                                 // insert "tmpRef = java.lang.System.out;"
                                 units.insertBefore(Jimple.v().newAssignStmt(
                                         tmpRef, Jimple.v().newStaticFieldRef(
                                                 Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
 
-                                /*
-                                units.insertBefore(Jimple.v().newCastExpr(invokeExpr.getArg(0),
-                                */
-
-
                                 // insert "tmpLong = 'HELLO';"
-                                /*
                                 units.insertBefore(Jimple.v().newAssignStmt(tmpString,
-                                        StringConstant.v("[NUDEBUG] setContentView: " + argStr)), u);
-                                        */
+                                        StringConstant.v("[NUDEBUG] Found URL (java.net.URL) : ")), u);
 
                                 // insert "tmpRef.println(tmpString);"
-                                //SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.Integer)");
-                                SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(int)");
-                                units.insertBefore(Jimple.v().newInvokeStmt(
-                                        Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), invokeExpr.getArg(0))), u);
+                                SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
 
-                                //check that we did not mess up the Jimple
+                                units.insertBefore(Jimple.v().newInvokeStmt(
+                                        Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.String)");
+                                units.insertBefore(Jimple.v().newInvokeStmt(
+                                        Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), v)), u);
+
                                 b.validate();
+
+                            } else if (invokeExpr.toString().contains("org.apache.http.client.methods.HttpPost") && invokeExpr.getMethod().toString().contains("init")) {
+                                if (invokeExpr.getMethod().getParameterType(0).getEscapedName().equals("java.lang.String")) {
+                                    Value v = invokeExpr.getArg(0);
+
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpRef2 = addTmpRef(b);
+                                    Local tmpString = addTmpString(b);
+
+                                    Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
+
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+
+                                    // insert "tmpLong = 'HELLO';"
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("[NUDEBUG] Found URL for HTTP POST : ")), u);
+
+                                    // insert "tmpRef.println(tmpString);"
+                                    SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.String)");
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), v)), u);
+
+                                    b.validate();
+                                }
+                            } else if (invokeExpr.toString().contains("org.apache.http.client.methods.HttpGet") && invokeExpr.getMethod().toString().contains("init")) {
+                                if (invokeExpr.getMethod().getParameterType(0).getEscapedName().equals("java.lang.String")) {
+                                    Value v = invokeExpr.getArg(0);
+
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpRef2 = addTmpRef(b);
+                                    Local tmpString = addTmpString(b);
+
+                                    Scene.v().addBasicClass("java.lang.System", SootClass.SIGNATURES);
+
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+
+                                    // insert "tmpLong = 'HELLO';"
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("[NUDEBUG] Found URL for HTTP GET: ")), u);
+
+                                    // insert "tmpRef.println(tmpString);"
+                                    SootMethod print = Scene.v().getSootClass("java.io.PrintStream").getMethod("void print(java.lang.String)");
+
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, print.makeRef(), tmpString)), u);
+
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.String)");
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), v)), u);
+
+                                    b.validate();
+                                }
                             }
                         }
-
                     });
                 }
             }
 
 
         }));
-
         soot.Main.main(args);
     }
 
@@ -110,4 +304,12 @@ public class AndroidInstrument {
         body.getLocals().add(tmpString);
         return tmpString;
     }
+
+    private static Local addTmpInt(Body body)
+    {
+        Local tmpInt = Jimple.v().newLocal("tmpInt", RefType.v("int"));
+        body.getLocals().add(tmpInt);
+        return tmpInt;
+    }
 }
+
