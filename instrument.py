085f893193d6ff68e2799fe2a0b82f2765847262
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2017-05-16 08:38:09 -0500

    add instrumenting

--- instrument.py
+++ instrument.py
@@ -3,16 +3,24 @@ import glob
 
 def run_soot(apk_name):
     os.system('java -cp soot-custom.jar:rt.jar AndroidInstrument  -android-jars /Users/jameswhang/android-platforms -process-dir /Users/jameswhang/Documents/school/eecs450/project/code/AndroidInstrument/apks/{} -allow-phantom-refs -d ./sootOutput -src-prec apk -output-format dex'.format(apk_name))
-#    os.system('java -cp soot-custom.jar:rt.jar AndroidInstrument  -android-jars /Users/jameswhang/android-platforms -process-dir /Users/jameswhang/Documents/school/eecs450/project/code/AndroidInstrument/apks/{} -allow-phantom-refs -d ./sootOutput3 -src-prec apk -f jimple'.format(apk_name))
+
+def run_soot_main(apk_name, output_dir):
+    os.system('java -cp soot-custom.jar:rt.jar soot.Main -android-jars /Users/jameswhang/android-platforms -process-dir /Users/jameswhang/Documents/school/eecs450/project/code/AndroidInstrument/apks/{} -allow-phantom-refs -d ./{} -src-prec apk -f jimple'.format(apk_name, output_dir))
+
+def run_soot_modify(apk_name, output_dir):
+    os.system('java -cp soot-custom.jar:rt.jar:android.jar AndroidInstrument -android-jars /Users/jameswhang/android-platforms -process-dir /Users/jameswhang/Documents/school/eecs450/project/code/AndroidInstrument/apks/{} -allow-phantom-refs -d ./{} -src-prec apk -f jimple'.format(apk_name, output_dir))
+
 
 
 
 if __name__ == '__main__':
+    #run_soot_modify('com.app_greenjobs.layout-400.apk', 'testdir')
     run_soot('com.app_greenjobs.layout-400.apk')
+    #run_soot('app-debug.apk')
+    #run_soot_modify('app-debug.apk', 'testdir')
     """
     apks = glob.glob('./apks/*.apk')
     for apk in apks:
         apk = apk.split('/')[-1]
-        print apk
         run_soot(apk)
     """
